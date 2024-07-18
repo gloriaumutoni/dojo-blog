bf973b6040f1bfc886287465c159ed93406117f1
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-18 15:29:13 +0200

    using props

--- src/BlogLists.js
+++ src/BlogLists.js
@@ -1,9 +1,12 @@
-const BlogLists = (props) => {
+const BlogLists = ({ blogs , title }) => {
     const blogs=props.blogs
+    const title=props.title
+    console.log(props)
     return (  
         <div className="home">
         {blogs.map((blog)=>(
             <div className='blog-preview' key={blog.id}>
+                <h2>{title}</h2>
                 <h2>{blog.title}</h2>
                 <p>{blog.author}</p>
             </div>
--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,40 @@
-import {useState} from 'react'
-import BlogLists from './BlogLists';
+import { useState } from "react";
+import BlogLists from "./BlogLists";
 
 const Home = () => {
-    // const [count,setCount] = useState(0)
-    // const handleClick=()=>{
-    //     console.log('clicked')
-    // }
-    const [blogs]=useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-    // const handleClickAgain=(name,e)=>{
-    //     console.log('clicked again by'+ name,e.target)
-    // }
-    return (
-      <div className="home">
-<BlogLists blogs={blogs}/>
-        {/* {blogs.map((blog)=>(
+  // const [count,setCount] = useState(0)
+  // const handleClick=()=>{
+  //     console.log('clicked')
+  // }
+  const [blogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  // const handleClickAgain=(name,e)=>{
+  //     console.log('clicked again by'+ name,e.target)
+  // }
+  return (
+    <div className="home">
+      <BlogLists blogs={blogs} title='All Blogs!'/>
+      {/* {blogs.map((blog)=>(
             <div className='blog-preview' key={blog.id}>
                 <h2>{blog.title}</h2>
                 <p>{blog.author}</p>
             </div>
         ))} */}
 
-        {/* <h2>Homepage</h2> */}
-        {/* <button onClick={handleClick}>Click me</button>
+      {/* <h2>Homepage</h2> */}
+      {/* <button onClick={handleClick}>Click me</button>
         <button onClick={()=>setCount(count+1)}> the count is {count}</button>
         <button onClick={(e) => handleClickAgain('mario',e)}>Click me again</button> */}
-        </div>
-    );
-  }
-   
-  export default Home;
+    </div>
+  );
+};
+
+export default Home;
