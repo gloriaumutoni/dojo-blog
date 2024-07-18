feaf6077543a48bb668dbe7adcccea46bb6fd31a
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-18 14:07:59 +0200

    using the hook function

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,27 @@ const Home = () => {
     const handleClick=()=>{
         console.log('clicked')
     }
+    const [blogs,setBlogs]=useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
     const handleClickAgain=(name,e)=>{
         console.log('clicked again by'+ name,e.target)
     }
     return (
       <div className="home">
-        <h2>Homepage</h2>
-        <button onClick={handleClick}>Click me</button>
+        {blogs.map((blog)=>(
+            <div className='' key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>{blog.author}</p>
+            </div>
+        ))}
+
+        {/* <h2>Homepage</h2> */}
+        {/* <button onClick={handleClick}>Click me</button>
         <button onClick={()=>setCount(count+1)}> the count is {count}</button>
-        <button onClick={(e) => handleClickAgain('mario',e)}>Click me again</button>
+        <button onClick={(e) => handleClickAgain('mario',e)}>Click me again</button> */}
         </div>
     );
   }
