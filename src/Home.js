2163e1d7000d6d6bee828ef1e648b0f9f06fcb08
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-18 14:13:09 +0200

    added styles to the home component

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
     return (
       <div className="home">
         {blogs.map((blog)=>(
-            <div className='' key={blog.id}>
+            <div className='blog-preview' key={blog.id}>
                 <h2>{blog.title}</h2>
                 <p>{blog.author}</p>
             </div>
