7367a3747c94d1e94ed5cea73e890d4efbb99b65
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-18 11:38:10 +0200

    adding events to a button

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick=()=>{
+        console.log('clicked')
+    }
     return (
       <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
       </div>
     );
   }
